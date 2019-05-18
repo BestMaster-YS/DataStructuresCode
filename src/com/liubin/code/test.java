14306956613354d2a103b24fd974201c6ca5312b
BestMaster-YS <1419831251@qq.ocm>
2019-05-18 23:23:07 +0800

    'test'

---
+++ src/com/liubin/code/test.java
@@ -0,0 +1,12 @@
+package com.liubin.code;
+
+/**
+ * @author BestMaster刘斌
+ * @date 2019/5/18 23:21
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
