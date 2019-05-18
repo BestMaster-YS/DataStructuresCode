989f91d1bacbfa03748201d08fa44a2f85ad8b29
BestMaster-YS <1419831251@qq.ocm>
2019-05-18 23:29:56 +0800

    'test'

--- src/com/liubin/code/Test.java
+++ src/com/liubin/code/Test.java
@@ -4,9 +4,9 @@ package com.liubin.code;
  * @author BestMaster刘斌
  * @date 2019/5/18 23:21
  */
-public class test {
+public class Test {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("Test");
     }
 }
